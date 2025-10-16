8161de3eac61d8483019c1b1885c5458e029e653
muhammetakkoc <akkoc.muhammet0@gmail.com>
2025-10-15 21:44:17 -0400

    Start(Menu) Scene added
    #8

--- Assets/Scripts/button Control.cs
+++ Assets/Scripts/button Control.cs
@@ -1,22 +1,45 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class buttonControl : MonoBehaviour
 {
+
+    public GameObject menuPanel;
+    public GameObject instructionPanel;
     
     void Start()
     {
-        
+        menuPanel.SetActive(true);
+        instructionPanel.SetActive(false);
     }
    public void StartButton()
     {
         SceneManager.LoadScene(1);
     }
     
-    void Update()
+    public void ShowInstructionPanel()
+    {
+        instructionPanel.SetActive(true);
+        menuPanel.SetActive(false);
+    }
+
+    public void Backbutton()
+    {
+        menuPanel.SetActive(true);
+        instructionPanel.SetActive(false);
+    }
+
+    public void QuitGame()
     {
         
+        Application.Quit();
+
+    }
+
+    public void MuteMusic()
+    {
+        AudioListener.volume = 0f;
     }
 }
